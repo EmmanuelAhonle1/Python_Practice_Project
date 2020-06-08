9aeae3e95768911f257d3cecade40ed7c2f24f32
Emmanuel Ahonle <emmanuelahonle101@gmail.com>
2020-06-08 08:56:33 -0500

    added a few more annotations to assist in Confluence explanation

--- Hangman Game.py
+++ Hangman Game.py
@@ -13,7 +13,7 @@ import random #picks random item froom list
 import threading  #uses timer 
 
 
-def animate():
+def animate(): #creates a loading animation where the slash spins clockwise
     for timer in range(0, 6):
         sys.stdout.write('\rYour game will begin shortly |')
         time.sleep(0.1)
@@ -26,12 +26,12 @@ def animate():
     sys.stdout.write('\r\n\n\n')
 
 loop = 0
-count = 0
-word = ''
-split_chosen_word = []
+count = 0 #counts how many times incorrect input is typed into q1
+word = ''      #randomly picked word from word_dictionary
+split_chosen_word = [] #displays randomly chosen word's characters as items in list
 tries_left = 0
 position = 0
-letter_bank = []
+letter_bank = [] #empty list which is appended every time a user input is given
 
 q1 = ''  #user input that determines difficulty
 #all functions being used in Hangman game
@@ -275,14 +275,15 @@ word_dictionary = [
 def letters_in_word	(split_chosen_word, user_input):
 	return split_chosen_word.count(user_input)
 	
-	
-word = random.choice(word_dictionary)
-def turn_word_string_into_list():
+
+#randomly picks item from list to use as Hangman word	
+word = random.choice(word_dictionary) #randomly picked word from word_dictionary
+def turn_word_string_into_list(): #turns the randomly picked word from list into a string by splitting up the characters in the word
 	for i in range(0, len(word)):
 		split_chosen_word.append(word[i])
 		i+=1
 
-#Question 1: Determines how long the words will be
+#Question 1: Determines how many failed attempts the player is given to guess the correct answer
 print("Welcome to my Python Hangman Game! \nIn this game, you will be tasked with guessing the letters in a word. \n Depending on the chosen difficulty, you have more or less tries to finish the word. Good luck!")
 time.sleep(3)
 
@@ -292,7 +293,7 @@ while True:
 	q1 = input('\nSelect a mode: \n \n [E]asy :) \n [M]edium :| \n [H]ard >:( \n Answer: ')
 	print(q1)
 
-		#enables easy mode: no timer 
+		#enables easy mode: sets tries_left equal to 10; 10 allowed incorrect guesses
 	if q1 in ['E', 'e']:
 		time.sleep(.5)
 		print("Ah a beginner I see! Don't worry, we'll give you ample time to  respond. You'll have 10 tries to type the correct answer \n")
@@ -302,7 +303,7 @@ while True:
 		tries_left = 10
 		break
 		
-#enables medium mode: 15 second timer
+#enables medium mode: sets tries_left equal to 5; 5 allowed incorrect guesses
 	elif q1 in ['M', 'm']:
 		time.sleep(.5)
 		print("Ah so you're a bit experienced! You will be given 5 tries to type a response\n")
@@ -312,7 +313,7 @@ while True:
 		time.sleep(1)
 		break
 	
-	#enables medium mode: 15 second timer
+	#enables medium mode: sets tries_left equal to 3; 3 allowed incorrect guesses
 	elif q1 in ['h', 'H']:
 		time.sleep(.5)
 		print("\nAh a daredevil I see! Let's see if you can put those skills to the test! You will be given 3 tries to guess the correct answer\n")
@@ -334,23 +335,24 @@ while True:
 turn_word_string_into_list()
 guessed_word = []
 
+#creates a list of '_' corresponding to the amount of items in the split_chosen_word list
 for letters in range(0, len(split_chosen_word)):
 	guessed_word.append('_')
 	
 	
 
-
+#function checks if user's typed in letter matches list of chosen words and responds if you have already used this letter
 def check_letter_bank():
 	if user_input in letter_bank:
 		print("You have already used this letter.")
 
 	
-animate()	
+animate()	#calls animate function
 
 print(word)
 print(guessed_word)
 
-while tries_left != 0:
+while tries_left != 0: #conditions for if game is still continuing and there is not a game over
 		
 	while True:
 			user_input = input('\nType in character OR guess the word: ')
@@ -363,16 +365,22 @@ while tries_left != 0:
 					letter_bank.append(user_input)
 					print(guessed_word)
 					
+					
+			#if the user guesses all the items in the list of characters from the random word, the code prints a message acknowledging correct word and exits terminal
 			if guessed_word == split_chosen_word:
 				print("\nCongrats! the word was '" + word + "'.")
 				
 				time.sleep(2)
 				exit()
-			
+				
+				
+			#if the user guesses the word, code prints a message acknowledging correct guess and exits terminal
 			elif user_input == word:
 				print("Congrats!, the word was " + word)
 				exit()
 			
+			
+			#if user_input isn't found in list of the randomly picked word, then the program prints an error message and takes off a point in tries_left
 			elif split_chosen_word.count(user_input) == 0:
 				tries_left -= 1
 				print("\nOof! Wrong answer! You have '" + str(tries_left) + "' tries left")
@@ -387,7 +395,7 @@ while tries_left != 0:
 		
 		
 		
-		
+		#if the player runs out of guessses, program prints 'Failed' message and the word that was meant to be guessed; exits terminal
 			if tries_left == 0:
 				print("\n \n \nPuzzle failed! Try again next time!")
 				print("The word was '" + word + "'.'")
